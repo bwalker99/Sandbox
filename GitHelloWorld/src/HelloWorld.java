bc84ee589aa6bcaaa8c7d0bf75adb73d30dbdc5b
rwalker <robert.walker@ubc.ca>
2013-10-11 13:20:48 -0700

    more

--- GitHelloWorld/src/HelloWorld.java
+++ GitHelloWorld/src/HelloWorld.java
@@ -2,7 +2,7 @@
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		System.out.println("Hello World to Git from Bob");
+		System.out.println("Hello World to Git from Bob with more");
 
 	}
 
