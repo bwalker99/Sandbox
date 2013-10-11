79503d2d9eccf914f7f99b10b5be69e20bb50db0
Bob <bwalker99@gmail.com>
2013-10-11 13:41:33 -0700

    Update HelloWorld.java

--- GitHelloWorld/src/HelloWorld.java
+++ GitHelloWorld/src/HelloWorld.java
@@ -1,8 +1,7 @@
-
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		System.out.println("Hello World to Git from Bob with more");
+		System.out.println("Hello World to Git from Bob with more - edit from Github");
 
 	}
 
