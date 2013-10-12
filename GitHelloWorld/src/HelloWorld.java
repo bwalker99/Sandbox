dd2c6fbe218f367953964925d010f057055eddbe
Bob <Bob@bobathome.ca>
2013-10-11 21:54:32 -0700

    commit from home

--- GitHelloWorld/src/HelloWorld.java
+++ GitHelloWorld/src/HelloWorld.java
@@ -2,6 +2,7 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		System.out.println("Hello World to Git from Bob with more - edit from Github");
+		System.out.println("Edit from home");
 
 	}
 
