7e88ac6e1aac06acc246ff9b059d193575db5ce2
rwalker <robert.walker@ubc.ca>
2013-10-28 15:07:49 -0700

    third committ

--- GitHelloWorld2/src/HelloWorld2.java
+++ GitHelloWorld2/src/HelloWorld2.java
@@ -4,6 +4,8 @@ public class HelloWorld2 {
 	public static void main(String[] args) {
 		System.out.println("Hello World 2 to Git from Bob");
 		System.out.println("Second Commit from Workstation...");
+		System.out.println("Third Commit from Workstation...");
+		
 		
 
 	}
