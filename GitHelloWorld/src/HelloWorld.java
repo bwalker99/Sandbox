8b2277178ee46e6770d31082ccd07f0ff654efcc
rwalker <robert.walker@ubc.ca>
2013-10-28 14:10:05 -0700

    from work 1

--- GitHelloWorld/src/HelloWorld.java
+++ GitHelloWorld/src/HelloWorld.java
@@ -3,7 +3,7 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Hello World to Git from Bob with more - edit from Github");
 		System.out.println("Edit from home - more edits from home...");
-		System.out.println("More edits from work...");
+		System.out.println("More edits from work...123");
 
 	}
 
