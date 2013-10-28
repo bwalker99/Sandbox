4a1872832b2f835dab72f2d7c6a7be98ee09e901
Brandon Nam <brandon.nam@ubc.ca>
2013-10-28 15:48:52 -0700

    Commit by brandon

--- GitHelloWorld3/src/HelloWorld3.java
+++ GitHelloWorld3/src/HelloWorld3.java
@@ -3,6 +3,7 @@ public class HelloWorld3 {
 	public static void main(String[] args) {
 		System.out.println("Hello World 3 to Git from Bob");
 		System.out.println("Next commit.");
+		System.out.println("edit by brandon");
 		
 	}
 
