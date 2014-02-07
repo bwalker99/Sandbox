e9775898122940b50394823f1031142fce0371ee
Bob Walker <robert.walker@ubc.ca>
2014-02-06 16:56:20 -0800

    minor changes all round

--- GitHelloWorld/src/HelloWorld.java
+++ GitHelloWorld/src/HelloWorld.java
@@ -1,3 +1,13 @@
+
+import java.util.HashMap;
+import java.util.Map;
+import java.io.PrintWriter;
+import java.io.File;
+
+import net.sf.json.JSON;
+import net.sf.json.JSONSerializer;
+
+
 public class HelloWorld {
 
 	public static void main(String[] args) {
@@ -6,6 +16,20 @@ public class HelloWorld {
 		System.out.println("More edits from work...123-456");
 		System.out.println("Edits from home");
 
+	}
+	
+	private void Test() { 
+		Map<String,String> resultMap = new HashMap<String,String>();
+	    resultMap.put("timestamp", String.valueOf(System.currentTimeMillis()));
+	    try { PrintWriter response = new PrintWriter(new File("dog.txt")); } catch (Exception e) { }
+	    //response.setContentType("text/x-json");
+	    //response.setCharacterEncoding("UTF-8");
+	    JSON json = JSONSerializer.toJSON(resultMap);
+	    
+	    // json.write(response.getWriter());
+	    
+		
+		
 	}
 
 }
